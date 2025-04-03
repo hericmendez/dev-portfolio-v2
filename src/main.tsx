45ab16776cc8356e1f518e024153ed4f5464e1c2
HericMendez <heric.mendez00@gmail.com>
2025-04-02 22:05:11 -0300

    added rpgui lib

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import "./styles/global.css";
-import "./styles/dist/rpgui.css";
+import "./styles/rpgui/rpgui.css";
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
